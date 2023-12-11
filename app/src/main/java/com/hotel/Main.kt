cba19abb33a877ce854b81752f1a4562d5765cb4
Umin <adorecamus@gmail.com>
2023-12-11 18:47:41 +0900

    Feat: check if room and date is available

--- app/src/main/java/com/hotel/Main.kt
+++ app/src/main/java/com/hotel/Main.kt
@@ -17,7 +17,7 @@ fun main() {
 
                 val name = validateInput("name").toString()
                 val roomNumber = validateInput("roomNumber").toString().toInt()
-                val date = validateDate().toList()
+                val date = validateDate(roomNumber, reservationHistory).toList()
 
                 val member = Member(name, balance)
                 memberList.add(member)
@@ -109,31 +109,69 @@ fun validateInput(type: String): Any? {
     }
 }
 
-fun validateDate(): Pair<LocalDate, LocalDate> {
+fun validateDate(roomNumber: Int, reservationHistory: ArrayList<Reservation>): Pair<LocalDate, LocalDate> {
+
+    val reservedRooms = reservationHistory.filter{ it.roomNumber == roomNumber}
+
     val formatter = DateTimeFormatter.BASIC_ISO_DATE
     val today: LocalDate = LocalDate.now()
+
+    var isAvailable: Boolean
+    var checkIn: LocalDate
     while (true) {
+        isAvailable = true
         println("체크인 날짜를 입력해주세요. 표기형식:20231208")
         try {
             var originCheckIn = readln()
-            val checkIn: LocalDate = LocalDate.parse(originCheckIn, formatter)
-            if (checkIn.isBefore(today)) {
+            val tempCheckIn: LocalDate = LocalDate.parse(originCheckIn, formatter)
+            if (tempCheckIn.isBefore(today)) {
                 println("지난 날짜에 체크인할 수 없습니다.")
                 continue
             }
-            while (true) {
-                println("체크아웃 날짜를 입력해주세요. 표기형식:20231208")
-                try {
-                    var originCheckOut = readln()
-                    val checkOut: LocalDate = LocalDate.parse(originCheckOut, formatter)
-                    if (checkOut.isAfter(checkIn)) {
-                        return Pair(checkIn, checkOut)
-                    } else {
-                        println("체크인 이후의 날짜에 체크아웃할 수 있습니다.")
-                    }
-                } catch (e: Exception) {
-                    println("잘못된 입력입니다.")
+            for (room in reservedRooms) {
+                if (tempCheckIn.isBefore(room.checkIn) || !tempCheckIn.isBefore(room.checkOut)) {
+                    continue
+                } else {
+                    println("해당 날짜에 이미 방을 사용 중입니다. 다른 날짜를 입력해주세요.")
+                    isAvailable = false
+                    break
                 }
+            }
+            if (isAvailable) {
+                checkIn = tempCheckIn
+                break
+            }
+        } catch (e: Exception) {
+            println("잘못된 입력입니다.")
+        }
+    }
+
+    var checkOut: LocalDate
+    while (true) {
+        isAvailable = true
+        println("체크아웃 날짜를 입력해주세요. 표기형식:20231208")
+        try {
+            var originCheckOut = readln()
+            val tempCheckOut = LocalDate.parse(originCheckOut, formatter)
+            if (!tempCheckOut.isAfter(checkIn)) {
+                println("체크인 이후의 날짜에 체크아웃할 수 있습니다.")
+                continue
+            }
+            for (room in reservedRooms) {
+                // 앞서 입력체크인이 예약체크인과 예약체크아웃 사이에 없음을 검증한 상태임
+                // 또 입력체크아웃은 입력체크인 이후임을 검증함
+                // 따라서 입력체크아웃은 예약체크인과 예약체크아웃 사이에 없음 (검증 필요없음)
+                // 입력체크인이 예약체크인보다 먼저인 경우에는 입력체크아웃이 예약체크인과 같거나 전이어야 함
+                // 입력체크인이 예약체크아웃보다 후인 경우는 검증 필요없음
+                if (checkIn.isBefore(room.checkIn) && tempCheckOut.isAfter(room.checkIn)) {
+                    println("해당 날짜에 이미 방을 사용 중입니다. 다른 날짜를 입력해주세요.")
+                    isAvailable = false
+                    break
+                }
+            }
+            if (isAvailable) {
+                checkOut = tempCheckOut
+                return Pair(checkIn, checkOut)
             }
         } catch (e: Exception) {
             println("잘못된 입력입니다.")
